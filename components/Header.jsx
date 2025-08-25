c3766dd8be11d3c1007d4acd3a5899c2c483d3fd
omarseddati16 <omarseddati16@gmail.com>
2025-08-25 16:13:11 +0200

    Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,42 +3,36 @@ import React from 'react'
 const Header = () => {
   return (
     <header>
-      <div className="container-fluid">
-        <div className="header-col">
-          <img src="../img/dc-logo.png" alt="" />
-        </div>
-        <div className="header-col">
-          <ul id='headerMenu'>
-            <li>
-              <a href="#">CHARACTERS</a>
-            </li>
-            <li>
-              <a href="#">COMICS</a>
-            </li>
-            <li>
-              <a href="#">MOVIES</a>
-            </li>
-            <li>
-              <a href="#">TV</a>
-            </li>
-            <li>
-              <a href="#">COLLECTIBLES</a>
-            </li>
-            <li>
-              <a href="#">VIDEOS</a>
-            </li>
-            <li>
-              <a href="#">FANS</a>
-            </li>
-            <li>
-              <a href="#">NEWS</a>
-            </li>
-            <li>
-              <a href="#">SHOP</a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <img src="../img/dc-logo.png" alt="" />
+      <ul id='headerMenu'>
+        <li>
+          <a href="#">CHARACTERS</a>
+        </li>
+        <li>
+          <a href="#">COMICS</a>
+        </li>
+        <li>
+          <a href="#">MOVIES</a>
+        </li>
+        <li>
+          <a href="#">TV</a>
+        </li>
+        <li>
+          <a href="#">COLLECTIBLES</a>
+        </li>
+        <li>
+          <a href="#">VIDEOS</a>
+        </li>
+        <li>
+          <a href="#">FANS</a>
+        </li>
+        <li>
+          <a href="#">NEWS</a>
+        </li>
+        <li>
+          <a href="#">SHOP</a>
+        </li>
+      </ul>
     </header>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 function App() {
-  return <Header />
+  return (<>
+    <Header />
+    <Main />
+  </>
+  )
 }
 
 export default App
