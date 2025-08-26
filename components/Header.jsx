c49a5025ef32635fe177ba90ee7556c0c584f8c5
omarseddati16 <omarseddati16@gmail.com>
2025-08-26 16:07:06 +0200

    fix e nav bar dinamica

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,37 +1,17 @@
 import React from 'react'
 
+const navBar = ["characters", "comics", "movies", "tv", "collectibles", "videos", "fans", "news", "shop"]
+
 const Header = () => {
   return (
     <header>
       <img src="../img/dc-logo.png" alt="" />
       <ul id='headerMenu'>
-        <li>
-          <a href="#">CHARACTERS</a>
-        </li>
-        <li>
-          <a href="#">COMICS</a>
-        </li>
-        <li>
-          <a href="#">MOVIES</a>
-        </li>
-        <li>
-          <a href="#">TV</a>
-        </li>
-        <li>
-          <a href="#">COLLECTIBLES</a>
-        </li>
-        <li>
-          <a href="#">VIDEOS</a>
-        </li>
-        <li>
-          <a href="#">FANS</a>
-        </li>
-        <li>
-          <a href="#">NEWS</a>
-        </li>
-        <li>
-          <a href="#">SHOP</a>
-        </li>
+        {navBar.map((item, index) => (
+          <li key={index}>
+            <a href="#">{item.toUpperCase()}</a>
+          </li>
+        ))}
       </ul>
     </header>
   )
